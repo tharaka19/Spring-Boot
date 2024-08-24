df0b9214fa55283281576b597ea75a1d36a7cb60
Shehan Tharaka <shehantharaka000@gmail.com>
2024-08-24 21:20:47 +0530

    Removed unsed import

--- src/main/java/com/demo/springboot/entity/Department.java
+++ src/main/java/com/demo/springboot/entity/Department.java
@@ -4,9 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.*;
 import lombok.Data;
-import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotBlank;
 
 @Data
 @Entity
